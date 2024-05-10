fd5825aafa0aaf404c2edc6c9a40e5b1c8fb16d0
Hamza-Bin-Aamir <148682978+Hamza-Bin-Aamir@users.noreply.github.com>
2024-05-10 05:03:28 +0500

    HTTP Hello World

---
+++ HTTP/server.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <winsock2.h> // Win Sock is the library that will help us do our HTTP request handling
+#pragma once
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	return 0;
+}
